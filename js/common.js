d993232230ca339792cf3b71540e6705d8cf6361
zidori <maestoso911@gmail.com>
2021-06-04 11:27:57 +0900

    blue bottle layout end
    일단 완성

--- js/common.js
+++ js/common.js
@@ -1,3 +1,12 @@
 $(function(){
-  //script 입력영역
+  // virtual classes arcodian
+  $(".curriculum").css({"display":"none"});
+  $(".course_box .course_name").on("click",function(){
+    console.log("click");
+    if($("+.curriculum",this).css("display") == "none"){
+      console.log("display none");
+      $(".course_box .curriculum").slideUp();
+      $("+.curriculum",this).slideDown();
+    }
+  });// course_name click
 });// document ready
